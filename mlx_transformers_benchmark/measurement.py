fbd233c3aec39afdbb86e38c4edb4b8f3176c41f
Auke Wiggers <wiggers.auke@gmail.com>
2025-03-29 17:32:16 +0100

    Add a measurement class for computing properties of interest.

---
+++ mlx_transformers_benchmark/measurement.py
@@ -0,0 +1,22 @@
+from typing import List
+import numpy as np
+
+
+class Measurement:
+    def __init__(self, measurements: List[float]):
+        self.measurements = np.array(measurements)
+
+        self.num_measurements = len(self.measurements)
+        self.median = np.median(self.measurements)
+        self.mean = np.mean(self.measurements)
+        self.stdev = np.std(self.measurements)
+
+    def __repr__(self):
+        return (
+            f"{self.__class__.__name__}("
+            f"\n  num_measurements={self.num_measurements}, "
+            f"\n  median={self.median:.4f}, "
+            f"\n  mean={self.mean:.4f}, "
+            f"\n  stdev={self.stdev:.4f} "
+            f"\n)"
+        )
