6fa14f3f008ca1349be34ca9b82e914f7d6bda5a
Auke Wiggers <wiggers.auke@gmail.com>
2025-04-15 14:29:25 +0200

    Add measurements container class.

--- mtb/measurement.py
+++ mtb/measurement.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import Dict, List
 
 import numpy as np
 
@@ -21,3 +21,53 @@ class Measurement:
             f"\n  std={self.std:.4f}, "
             f"\n)"
         )
+
+
+class Measurements:
+    """Container class for measurements."""
+
+    def __init__(self):
+        self._measurements = None
+        self._num_measurements = 0
+
+    def add(self, measurement: Dict):
+        # initialize container if needed
+        if self._measurements is None:
+            self._measurements = dict()
+            for key in measurement:
+                self._measurements[key] = []
+
+        # add values to the container
+        for key in self._measurements:
+            if key not in measurement:
+                raise KeyError(f"Key {key} not found in measurements")
+
+            self._measurements[key].append(measurement[key])
+
+        self._num_measurements += 1
+        return
+
+    def get_mean(self, key: str) -> float:
+        if key in self._measurements:
+            return np.mean(self._measurements[key])
+        else:
+            raise KeyError(
+                f"Key {key} not found in measurements, must be one of {list(self._measurements.keys())}"
+            )
+
+    def get_means(self) -> Dict[str, float]:
+        return {key: self.get_mean(key) for key in self._measurements}
+
+    def reset(self):
+        self._measurements = None
+        self._num_measurements = 0
+
+    def __repr__(self) -> str:
+        tostring = (
+            f"{self.__class__.__name__}("
+            f"\n  num_measurements={self._num_measurements},"
+        )
+        for key in self._measurements:
+            tostring += f"\n  {key}={self.get_mean(key):.4f},"
+        tostring += "\n)"
+        return tostring
--- tests/test_measurement.py
+++ tests/test_measurement.py
@@ -1,6 +1,7 @@
 import numpy as np
+import pytest
 
-from mtb.measurement import Measurement
+from mtb.measurement import Measurement, Measurements
 
 
 def test_measurement():
@@ -20,3 +21,58 @@ def test_measurement():
         "\n  std=0.1700, "
         "\n)"
     )
+
+
+def test_measurements():
+    measurements = Measurements()
+    measurements.add(
+        {
+            "runtime": 0.1,
+            "memory": 10.0,
+        }
+    )
+    measurements.add(
+        {
+            "runtime": 0.3,
+            "memory": 20.0,
+        }
+    )
+
+    assert measurements._num_measurements == 2
+    assert measurements.get_mean("runtime") == 0.2
+    assert measurements.get_mean("memory") == 15.0
+
+    expected_means = {
+        "runtime": 0.2,
+        "memory": 15.0,
+    }
+    assert measurements.get_means() == expected_means
+
+    assert str(measurements) == (
+        "Measurements("
+        "\n  num_measurements=2,"
+        "\n  runtime=0.2000,"
+        "\n  memory=15.0000,"
+        "\n)"
+    )
+
+    measurements.reset()
+    assert measurements._num_measurements == 0
+
+
+def test_measurements_errors():
+    measurements = Measurements()
+    measurements.add(
+        {
+            "runtime": 0.1,
+            "memory": 10.0,
+        }
+    )
+
+    # incomplete entry
+    with pytest.raises(KeyError):
+        measurements.add({"runtime": 0.2})
+
+    # key not present in measurements
+    with pytest.raises(KeyError):
+        measurements.get_mean("invalid_key")
