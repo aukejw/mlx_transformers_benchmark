1dbfe0ae433daee34c1b75267c08ab19e70ddb47
Auke Wiggers <wiggers.auke@gmail.com>
2025-04-16 18:53:13 +0200

    Only set the path if it isn't already set.

--- mtb/hf_utils.py
+++ mtb/hf_utils.py
@@ -10,12 +10,12 @@ hf_home = os.environ.get("HF_HOME", "./hf_cache")
 def set_hf_home(path: Union[str, Path] = "./hf_cache"):
     """Set the HF_HOME environment variable to a specific path."""
     global hf_home
-    hf_home = str(path)
+    if hf_home != str(path):
+        hf_home = str(path)
+        os.environ["HF_HOME"] = hf_home
+        print(f"HF_HOME set to {hf_home}")
 
-    os.environ["HF_HOME"] = hf_home
-    print(f"HF_HOME set to {hf_home}")
-
-    disable_progress_bar()
+        disable_progress_bar()
 
 
 def get_hf_home() -> str:
