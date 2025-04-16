e888ceb447aa07bcbf0485b1a3e05f3b97b6dfcc
Auke Wiggers <wiggers.auke@gmail.com>
2025-04-16 16:12:54 +0200

    Disable progress bars for transformers.

--- mtb/hf_utils.py
+++ mtb/hf_utils.py
@@ -2,6 +2,8 @@ import os
 from pathlib import Path
 from typing import Union
 
+from transformers.utils.logging import disable_progress_bar
+
 hf_home = os.environ.get("HF_HOME", "./hf_cache")
 
 
@@ -9,8 +11,11 @@ def set_hf_home(path: Union[str, Path] = "./hf_cache"):
     """Set the HF_HOME environment variable to a specific path."""
     global hf_home
     hf_home = str(path)
+
     os.environ["HF_HOME"] = hf_home
     print(f"HF_HOME set to {hf_home}")
+
+    disable_progress_bar()
 
 
 def get_hf_home() -> str:
