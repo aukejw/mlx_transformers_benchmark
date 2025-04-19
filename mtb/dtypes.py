19c8561fd89e85754c072fa9deac0dc1190bff48
Auke Wiggers <wiggers.auke@gmail.com>
2025-04-19 20:37:19 +0200

    Add placeholders for lower precision dtypes. We'll have to go for int4 6 8 models at some point.

--- mtb/dtypes.py
+++ mtb/dtypes.py
@@ -7,6 +7,9 @@ def get_torch_dtype(dtype_str: str) -> torch.dtype:
         float32=torch.float32,
         bfloat16=torch.bfloat16,
         float16=torch.float16,
+        int8=torch.int8,
+        int6="torch.int6",  # doesn't exist
+        int4="torch.int4",  # doesn't exist
     )[dtype_str]
     return dtype
 
@@ -16,5 +19,8 @@ def get_mlx_dtype(dtype_str) -> mx.Dtype:
         float32=mx.float32,
         bfloat16=mx.bfloat16,
         float16=mx.float16,
+        int8=mx.int8,
+        int6="mx.int6",  # doesn't exist
+        int4="mx.int4",  # doesn't exist
     )[dtype_str]
     return dtype
