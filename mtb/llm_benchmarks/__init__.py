6f8e16a77984404d7f07c71a299d2a5b9bae5843
Auke Wiggers <wiggers.auke@gmail.com>
2025-04-21 15:27:42 +0200

    Verbose download.

--- mtb/llm_benchmarks/__init__.py
+++ mtb/llm_benchmarks/__init__.py
@@ -1,4 +1,8 @@
-from mtb.llm_benchmarks.gemma import Gemma3_1B_it_Benchmark, Gemma3_4B_it_Benchmark
+from mtb.llm_benchmarks.gemma import (
+    Gemma3_1B_it_Benchmark,
+    Gemma3_4B_it_Benchmark,
+    Gemma3_4B_it_QAT_Benchmark,
+)
 from mtb.llm_benchmarks.qwen import (
     Qwen2p5_0p5B_it_Benchmark,
     Qwen2p5_3B_it_Benchmark,
@@ -10,6 +14,7 @@ __all__ = [
     # Gemma 3 models
     "Gemma3_1B_it_Benchmark",
     "Gemma3_4B_it_Benchmark",
+    "Gemma3_4B_it_QAT_Benchmark",
     # Qwen 2.5 models
     "Qwen2p5_0p5B_it_Benchmark",
     "Qwen2p5_Coder_0p5B_it_Benchmark",
