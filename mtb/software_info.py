2dbbeab10392a5c13262586b6d6035eb72e429b5
Auke Wiggers <wiggers.auke@gmail.com>
2025-04-24 20:33:06 +0200

    lmstudio version is not that useful tbh, but might as well..

--- mtb/software_info.py
+++ mtb/software_info.py
@@ -1,5 +1,6 @@
 from typing import Dict
 
+import lmstudio
 import mlx.core as mx
 import mlx_lm
 import torch
@@ -18,4 +19,16 @@ def get_mlx_version() -> Dict:
     return dict(
         mlx_version=mx.__version__,
         mlx_lm_version=mlx_lm.__version__,
+    )
+
+
+def get_lmstudio_version() -> Dict:
+    """Get the current lmstudio version and version of important dependencies.
+
+    Note: this is just the version of the python API, and doesn't say anything
+    about llama.cpp, LMStudio, or the model files.
+
+    """
+    return dict(
+        lmstudio_version=lmstudio.__version__,
     )
