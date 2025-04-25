f42fec703953281540c5b4a4990dd7a38aac7002
Auke Wiggers <wiggers.auke@gmail.com>
2025-04-25 11:06:37 +0200

    lms is not available if lms raises a FileNotFoundError.

--- mtb/lmstudio_utils.py
+++ mtb/lmstudio_utils.py
@@ -5,11 +5,16 @@ def check_lms_server_running() -> bool:
     """Return if the LM Studio server is running."""
 
     command = ["lms", "server", "status"]
-    response = (
-        subprocess.check_output(command, stderr=subprocess.STDOUT)
-        .decode("utf-8")
-        .strip()
-    )
+    try:
+        response = (
+            subprocess.check_output(command, stderr=subprocess.STDOUT)
+            .decode("utf-8")
+            .strip()
+        )
+    except FileNotFoundError:
+        # Likely, lms is not available
+        return False
+
     success_message = "The server is running on port"
     if response.startswith(success_message):
         return True
